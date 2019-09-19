1fa4381f0b5c7a02088911a8c238f5a31fb647cd
ndeahl1999 <deahln@rpi.edu>
2019-09-19 16:44:36 -0400

    packet structs and port setup

---
+++ HW1/README.txt
@@ -0,0 +1,1 @@
+Noah Deahl, Andrew Qu 
--- HW1/hw1.c
+++ HW1/hw1.c
@@ -11,23 +11,94 @@ extern "C" {
 #include "../../unpv13e/lib/unp.h"
 }
 //#include "../../unpv13e/lib/unp.h"
+
+/*  Request Packet has format:
+    
+      2 bytes     string    1 byte   string   1 byte
+     ------------------------------------------------   
+    | Opcode |  Filename  |   0  |    Mode    |   0  |
+     ------------------------------------------------
+*/
+typedef struct{
+    char * opcode;
+    char * filename;
+    char * mode; 
+    
+}request_packet;
+
+/*  DATA Packet has format:
+    
+     2 bytes     2 bytes      n bytes
+     ----------------------------------
+    | Opcode |   Block #  |   Data     |
+     ----------------------------------
+*/
+typedef struct{
+    char * opcode;
+    char * block;
+    char * data; 
+    
+}data_packet;
+
+/*  ACK Packet has format:
+    
+      2 bytes     2 bytes
+     ---------------------
+    | Opcode |   Block #  |
+     ---------------------
+*/
+typedef struct{
+    char * opcode;
+    char * block;
+}ack_packet;
+
+/*  ERROR Packet has format:
+    
+      2 bytes     2 bytes      string    1 byte
+     -----------------------------------------
+    | Opcode |  ErrorCode |   ErrMsg   |   0  |
+     -----------------------------------------
+*/
+typedef struct{
+    char * opcode;
+    char * block;
+    char * data; 
+    
+}error_packet;
+
+
+unsigned short port_start;
+unsigned short port_end;
+unsigned short current_port;
+
+unsigned short find_port(){
+    current_port++;
+    if(current_port > port_end){
+        fprintf(stderr, "ERROR: Ports outside of given range\n");
+        return EXIT_FAILURE;
+    }else
+    {
+        return current_port;
+    }
+    
+}
+
 int main(int argc, char ** argv)
 {
 
     if(argc < 2){
         
-        perror("ERROR: ");
+        fprintf(stderr, "ERROR:Invalid number of arguments\n");
         return EXIT_FAILURE;
     }
 
-    unsigned short port_start = htons(atoi(argv[1]));
-    unsigned short port_end = htons(atoi(argv[2]));
+    port_start = htons(atoi(argv[1]));
+    port_end = htons(atoi(argv[2]));
+    current_port = port_start;
 
     int listenfd, connfd;
     pid_t childpid;
     socklen_t clilen;
-    struct sockaddr_in client, server;
-
     struct sockaddr_in clientaddr, servaddr;
 
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
@@ -35,14 +106,14 @@ int main(int argc, char ** argv)
     bzero(&servaddr, sizeof(servaddr));
 	  servaddr.sin_family      = AF_INET;
 	  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	  servaddr.sin_port        = htons(SERV_PORT);
+	  servaddr.sin_port        = htons(port_start);
 
     Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-	  Listen(listenfd, LISTENQ);
+	Listen(listenfd, LISTENQ);
 
     for( ; ; ){
-        clilen = sizeof(client);
+        clilen = sizeof(clientaddr);
         if ( (connfd = accept(listenfd, (SA *) &clientaddr, &clilen)) < 0) {
 			if (errno == EINTR)
 				continue;		/* back to for() */
