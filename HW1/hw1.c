9ede165c636c2c09de4ff0c69cf30729880764ce
Andrew Qu <qua@rpi.edu>
2019-09-23 16:04:18 -0400

    newest

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -1,40 +1,270 @@
-/* 
-    Noah Deahl
-    Andrew Qu
-    Net Prog HW1
-*/
-
-//#include "unp.h"
-//
-extern "C" {
-//#include "unp.h"
-#include "../../unpv13e/lib/unp.h"
-}
-//#include "../../unpv13e/lib/unp.h"
-
-#include "structs.h"
+#include "unp.h"
 
 
-
-
-// use later to make sure there doesn't exceed the maximum # of connections
+// keep track of the ports
+int current_port;
 int max_port;
 
-// use to generate new unique port to recvfrom
-int current_port;
+
+// SIGCHLD
+void handler(){
+    pid_t pid;
+    int stat;
+
+    while((pid = waitpid(-1, &stat, WNOHANG)) > 0){
+        printf("child %d terminatied\n", pid);
+    }
+}
 
 
-void handle_write(struct sockaddr_in* sock,char* buffer, int buffer_length);
-void handle_read(struct sockaddr_in* sock,char* buffer, int buffer_length);
+// SIGALRM
+void sig_alarm(){
+  alarm(10);
+}
 
 
+void get_request(int listenfd, struct sockaddr_in * servaddr, char * buffer){
+  int tid = ntohs(servaddr->sin_port);
+  // printf("that port is %d\n", tid);
+  char filename[80];
+  int block = 0;
+  char data[517];
+  int current_len;
 
-int send_ack(int listenfd, uint16_t block, struct sockaddr_in * sock, socklen_t sock_len);
+  unsigned short * opcode_ptr = (unsigned short *)buffer;
+  int n;
+  //get file
+  strcpy(filename, buffer+2);
+
+  // printf("file name is %s\n", filename);
+  FILE * f = fopen(filename, "r");
+
+  // couldn't open
+  if(f == NULL){ 
+    *opcode_ptr = htons(5);
+    *(opcode_ptr+1) = htons(1);
+    *(buffer+4) = 0;
+    //send an error and terminate
+    n = sendto(listenfd, buffer, 5, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+    return;
+  }
+
+  while(1){
+    block++;
+
+    // append to after the op code
+    *opcode_ptr = htons(3);
+    *(opcode_ptr+1) = htons(block);
+    for(n = 4; n < 516; n++){
+        if(feof(f)) break;
+        buffer[n] = fgetc(f);
+    }
+
+    // create the full packet
+    for(int i = 0; i < 517; i++)
+      data[i] = buffer[i];
+    current_len = n;
 
 
-int main(int argc, char ** argv)
-{
+    //send
+    n = sendto(listenfd, buffer, n, 0,(struct sockaddr*) servaddr, sizeof(*servaddr));
 
+    int count = 0;
+
+
+    socklen_t sockaddr_len;
+    // get ack
+    while(1){
+      n = recvfrom(listenfd, buffer, 517, 0, (struct sockaddr *)&servaddr, &sockaddr_len);
+      if(n < 0) {
+        if(errno == EINTR) continue;
+
+        // wait a second
+        if(errno == EWOULDBLOCK){
+          if(++count >= 10){
+            printf("transaction timed out\n");
+            break;
+          }
+
+          // 
+          //for(int i = 0; i < 517; i++)
+           // buffer[i] = data[i];
+          n = sendto(listenfd, data, current_len,0,(struct sockaddr*) servaddr, sizeof(*servaddr));
+        }
+        perror("recvfrom");
+        exit(-1);
+      }else{
+        break;
+      }
+    }
+    //check the tid
+    if(ntohs(servaddr->sin_port) != tid){
+      //different client
+      *opcode_ptr = htons(5);
+      *(opcode_ptr+1) = htons(5);
+      *(buffer+4) = 0;
+      n = sendto(listenfd, buffer, 5, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+    }
+
+    //reset the timout counter
+    count = 0;
+
+
+    // make sure it's not ack
+    // that its RRQ
+    if(ntohs(*opcode_ptr) != 4) {
+      if(ntohs(*opcode_ptr) == 1){
+
+        // reset packet
+        // for(int i = 0; i < 517; i++)
+        //   buffer[i] = data[i];
+        n = sendto(listenfd, data, current_len, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+      }
+    }
+
+    // done sending
+    if(current_len < 516)
+        break;
+  }
+
+  fclose(f);
+}
+
+void put_reqest(int listenfd, struct sockaddr_in * servaddr, char * buffer){
+  int tid = ntohs(servaddr->sin_port);
+  char filename[80];
+  int block = 0;
+  char data[517];
+  int last_block = 0;
+
+  //get file
+  strcpy(filename, buffer+2);
+  FILE * f = fopen(filename, "w");
+
+  unsigned short * opcode_ptr = (unsigned short *)buffer;
+
+  //set the op code 
+  *opcode_ptr = htons(4);
+  *(opcode_ptr + 1) = htons(block);
+
+  //store contents of this packet
+  for(int i = 0; i < 517; i++)
+      data[i] = buffer[i];
+  int current_len = 4;
+
+  //send the first ack to begin receiving data
+  int n = sendto(listenfd, buffer, 4, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+  int done = 0;
+
+  socklen_t sockaddr_len;
+  int count = 0;
+
+  // keep the loop going until all sent
+  while(!done){
+
+    // receive data
+    n = recvfrom(listenfd, buffer, 517, 0, (struct sockaddr *)&servaddr, &sockaddr_len);
+    if(n < 0) {
+      if(errno == EINTR) continue;
+      //1 second timeout
+      if(errno == EWOULDBLOCK){
+        if(++count >= 10){
+          printf("transaction timed out\n");
+          break;
+        }
+        //restore last packet
+        for(int i = 0; i < 517; i++)
+          buffer[i] = data[i];
+        n = sendto(listenfd, buffer, current_len, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+        continue;
+      }
+      perror("recvfrom");
+      exit(-1);
+    }
+    
+    // confirm it's from the right TID
+    if(ntohs(servaddr->sin_port) != tid){
+      //different client
+      *opcode_ptr = htons(5);
+      *(opcode_ptr+1) = htons(5);
+      *(buffer+4) = 0;
+      
+      n = sendto(listenfd, buffer, 5, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+
+      // otherwise try again
+      continue;
+    }
+
+    //reset the timout counter
+    count = 0;
+
+    // make sure its data packet
+    if(ntohs(*opcode_ptr) != 3) {
+      if(ntohs(*opcode_ptr) == 2){
+
+        // reset packet
+        for(int i = 0; i < 517; i++)
+          buffer[i] = data[i];
+        n = sendto(listenfd, buffer, current_len, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+      }
+      continue;
+    }
+
+
+    // get the block
+    block = ntohs(*(opcode_ptr+1));
+    if(block == last_block){
+
+      // set data for sending
+      for(int i = 0; i < 517; i++)
+        buffer[i] = data[i];
+      
+        n = sendto(listenfd, buffer, current_len, 0, (struct sockaddr*) servaddr, sizeof(*servaddr));
+      continue;
+    }
+
+    buffer[n] = '\0';
+    fprintf(f, "%s\n", buffer+4);
+
+    // done sending
+    if(n < 516) 
+      done = 1;
+
+    // send ack
+    *opcode_ptr = htons(4);
+    *(opcode_ptr+1) = htons(block);
+
+    // send packet
+    for(int i = 0; i < 517; i++)
+      data[i] = buffer[i];
+    current_len = 4;
+
+
+    n = sendto(listenfd, buffer, 4, 0, (struct sockaddr*)servaddr, sizeof(*servaddr));
+  }    
+  fclose(f);  
+}
+
+int main(int argc, char** argv) {
+    int n;
+    char buffer[517];
+    socklen_t sockaddr_len;
+    struct sigaction act;
+    
+
+    // interupt sigalarm
+    act.sa_handler = sig_alarm;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGALRM, &act, NULL);
+
+
+    // interupt sigchld
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, NULL);
+    
     if(argc < 2){
         fprintf(stderr, "ERROR:Invalid number of arguments\n");
         return EXIT_FAILURE;
@@ -46,251 +276,106 @@ int main(int argc, char ** argv)
     
     current_port = port_start;
 
+
     int listenfd, connfd;
     pid_t childpid;
-    socklen_t clilen;
     struct sockaddr_in servaddr;
+
+    sockaddr_len = sizeof(servaddr);
 
     // initialize the socket
     if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ){
       perror("socket failed");
       exit(0);
     }
+    memset(&servaddr, 0, sockaddr_len);
 
-    bzero(&servaddr, sizeof(servaddr));
-	  servaddr.sin_family      = AF_INET;
+    servaddr.sin_family      = AF_INET;
 	  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	  servaddr.sin_port        = htons(port_start);
 
+    printf("%d\n", port_start);
+
     // bind to port
-    if(bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
+    if(bind(listenfd, (SA *) &servaddr, sockaddr_len) < 0){
       perror("bind failed");
       exit(0);
     }
-    
-    // dispaly the address listening to currently
-    printf("%d\n", ntohs(servaddr.sin_port));
 
 
-    // start to receive
-    int msg_len;
-    char buffer[1024];
+    // for later
     unsigned short int opcode;
     unsigned short int * opcode_ptr;
 
-    for( ; ; ){
 
-      struct sockaddr_in client_socket;
-      socklen_t slen = sizeof(client_socket);
+    while(1) {
+      n = recvfrom(listenfd, buffer, 517, 0, (struct sockaddr *)&servaddr, &sockaddr_len);
+      if(n < 0) {
 
-      //get a message
-      msg_len = recvfrom(listenfd, &buffer, sizeof(buffer), 0, (struct sockaddr *)&client_socket, &slen);
-      if(msg_len < 0){
-        if(errno == EINTR)
+        // to fix interrupting the system call
+        if(errno == EINTR) 
           continue;
-        perror("recvfrom error");
-        exit(0);
+        perror("recvfrom\n");
+        exit(-1);
       }
-
-
-      // got message successfully
-      // get the type of message
-      opcode_ptr = (unsigned short int *) buffer;
+      /* check the opcode */
+      opcode_ptr = (unsigned short *)buffer;
       opcode = ntohs(*opcode_ptr);
-      printf("the opcode message is : %d\n", opcode);
-
-      if(opcode != 1 && opcode != 2){
-      
+      if(opcode != 1 && opcode != 2) {
+        
+        // send error code
+        // can't accept non-RRQ or non-WRQ here
         *opcode_ptr = htons(5);
-        *(opcode_ptr+1) = htons(4);
-        msg_len = sendto(listenfd, &buffer, 5, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-        if(msg_len < 0){
-          perror("failed to send");
-          return 1;
+        *(opcode_ptr + 1) = htons(4);
+        *(buffer + 4) = 0;
+        n = sendto(listenfd, buffer, 5, 0, (struct sockaddr *)&servaddr, sockaddr_len);
+        if(n < 0) {
+          perror("sendto");
+          exit(-1);
         }
-      
       }
-      else{
-        if(fork() == 0){
+      else {
 
+        // child
+        if(fork() == 0) {
           close(listenfd);
-
-          if(opcode == 1)
-            handle_read(&client_socket, buffer, 1024);
-          if(opcode == 2)
-            handle_write(&client_socket, buffer, 1024);
           break;
-        
         }
       }
     }
+
+    
+    struct sockaddr_in sock_info;
+    sock_info.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_info.sin_port = htons(++current_port);
+    sock_info.sin_family = AF_INET;
+
+    if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(-1);
+    }
+
+    if(bind(listenfd, (struct sockaddr *)&sock_info, sockaddr_len) < 0) {
+        perror("%d\n" + sock_info.sin_port);
+        perror("bind error");
+        exit(-1);
+    }
+
+    struct timeval timeout_interval;
+    //set a 1 second timeout for receiving from the socket
+    timeout_interval.tv_sec = 1;
+    timeout_interval.tv_usec = 0;
+    setsockopt(listenfd, SOL_SOCKET, SO_RCVTIMEO, &timeout_interval, sizeof(timeout_interval));
+
+    // if RRQ
+    if(opcode == 1)
+      get_request(listenfd, &servaddr, buffer);
+
+    // if WRQ
+    if(opcode == 2) 
+      put_reqest(listenfd, &servaddr, buffer);
+
+    close(listenfd);
+    
     return 0;
-
 }
-
-
-
-void handle_write(struct sockaddr_in* sock, char* buffer, int buffer_length){
-
-
-
-
-}
-
-
-void handle_read(struct sockaddr_in* sock, char* buffer, int buffer_length){
-
-  
-  char file_name[80];
-  strcpy(file_name, buffer+2);
-
-  printf("file name is [%s]\n", file_name);
-
-
-
-  // set timeout
-  struct timeval tv;
-  tv.tv_sec = 1 ;
-  tv.tv_usec = 0;
-  
-  //create socket 
-  int listenfd;
-  if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-    perror("socket failed");
-    return;
-  }
-
-
-  // add some options
-  if(setsockopt(listenfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) <0){
-    perror("failed to set sock");
-    return;
-  }
-
-
-  // open file for reading
-  FILE *fd;
-  fd = fopen(file_name, "r");
-  if(fd == NULL){
-    perror("file couldn't");
-    return;
-  }
-
-
-
-  // main loop to read file and pass it
-  int block = 0;
-  int current_len = 0;
-
-  bool done = false;
-
-  int msg_len;
-  while(!done){
-  
-    char data[512];
-    current_len = fread(data, 1,sizeof(data), fd);
-    data[current_len] = '\0';
-    block++;
-
-    if(current_len < 512)
-      done = true;
-
-    int count = 10;
-    
-    data_packet message;
-    
-    
-    while(count > 0){
-
-      message.op_code = htons(3);
-      message.block = htons(block);
-      memcpy(message.data, data, current_len);
-      printf("data read: %s\n", data);
-
-      
-
-      // fix this send
-      // TODO
-      if((msg_len = sendto(listenfd, &message, 4 + current_len, 0, (struct sockaddr *) sock, sizeof(sock))) == 0){
-        perror("failed to send");
-        return;
-      }
-      
-      if(msg_len < 0){
-      
-        perror("no send");
-        return;
-      }
-
-
-      int recv_len;
-      socklen_t holder = sizeof(*sock);
-      if((recv_len = recvfrom(listenfd, buffer, sizeof(*buffer), 0, (struct sockaddr *) sock, &holder)) <0 && errno != EAGAIN){
-      
-        perror("receive error");
-      }
-
-      if( recv_len >= 4){
-      
-        count = -1;
-      }
-
-      if(count != 1){
-      
-        if(recv_len < 4 && recv_len >=0){
-          const char *req_s = "requset was not valid";
-          msg_len = sendto(listenfd, &req_s, strlen(req_s)+4, 0, (struct sockaddr*) sock, holder);
-          return;
-        }
-        if(errno == EAGAIN){
-        
-          printf("errno\n");
-          return;
-        }
-      }
-
-
-      count--;
-
-      
-    }
-    if(count == 0){
-      printf("timed out\n");
-      return;
-    }
-
-
-  }
-
-  fclose(fd);
-  close(listenfd);
-
-
-
-
-
-}
-
-
-
-// dedicated function to sending acknowledgement 
-int send_ack(int listenfd, uint16_t block, struct sockaddr_in * sock, socklen_t sock_len){
-
-  ack_packet packet;
-
-  packet.op_code= htons(4);
-  packet.block = htons(block);
-
-  ssize_t bytes_sent;
-  if((bytes_sent = sendto(listenfd, &packet, sizeof(packet), 0, (struct sockaddr *) sock, sock_len)) < 0 ){
-    perror("sendto failed");
-    exit(0);
-  }
-
-  return bytes_sent;
-
-}
-
-
-
