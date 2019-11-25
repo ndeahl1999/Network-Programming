06ceb5c1eb7f20ae7d9932335f215362e6a3ebe1
Andrew Qu <qua@rpi.edu>
2019-11-25 10:18:14 -0500

    created server listening

--- HW4/hw4_starter.py
+++ HW4/hw4_starter.py
@@ -8,6 +8,13 @@ import grpc
 import csci4220_hw4_pb2
 import csci4220_hw4_pb2_grpc
 
+
+class HashTable(csci4220_hw4_pb2_grpc.KadImplServicer):
+
+    def First(self, request, context):
+        print("gottem")
+        pass
+
 def run():
     if len(sys.argv) != 4:
         print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
@@ -22,6 +29,12 @@ def run():
     When you are done with a channel you should call .close() on the channel.
     Submitty may kill your program if you have too many file descriptors open
     at the same time.'''
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    csci4220_hw4_pb2_grpc.add_KadImplServicer_to_server(HashTable(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
     # remote_addr = socket.gethostbyname(remote_addr_string)
     # remote_port = int(remote_port_string)
     # channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
