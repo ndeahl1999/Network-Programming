778e540861f71c3eb72784c5dc9f40a5fb78729d
Andrew Qu <qua@rpi.edu>
2019-11-26 21:43:09 -0500

    initial peer to peer communication started, can BOOTSTRAP peerID port  to talk to the port

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -16,6 +16,16 @@ class HashTable(csci4220_hw4_pb2_grpc.KadImplServicer):
         print("gottem")
         pass
 
+
+    '''
+
+    This should return a NodeList object
+
+    '''
+    def FindNode(self, request, context):
+        print("implemented")
+        return 1
+
 def run():
     if len(sys.argv) != 4:
         print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
@@ -31,30 +41,50 @@ def run():
     Submitty may kill your program if you have too many file descriptors open
     at the same time.'''
 
-    # server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # csci4220_hw4_pb2_grpc.add_KadImplServicer_to_server(HashTable(), server)
-    # server.add_insecure_port('[::]:50051')
-    # server.start()
     # server.wait_for_termination()
     # remote_addr = socket.gethostbyname(remote_addr_string)
     # remote_port = int(remote_port_string)
     # channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
 
+    # start the server in the background
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    csci4220_hw4_pb2_grpc.add_KadImplServicer_to_server(HashTable(), server)
+    server.add_insecure_port('[::]:'+my_port)
+    server.start()
+
+    
+
+    # initializing the buckets 2^k
     buckets = pow(2, k)
     k_buckets = [None] * 4
-    print(k_buckets)
 
+
+    # main listening loop
     while (True):
         stdin = str(input())
         arguments = stdin.split(" ")
+        # connect to another peer
         if (arguments[0] == "BOOTSTRAP"):
+            peer_id = arguments[1]
+            peer_port = arguments[2]
             print("handle bootstrapping here")
+
+            # temporarily connect with them to get 
+            with grpc.insecure_channel('localhost:'+peer_port) as channel:
+                stub = csci4220_hw4_pb2_grpc.KadImplStub(channel)
+                obj = csci4220_hw4_pb2.IDKey(node=csci4220_hw4_pb2.Node(id=local_id, port=int(my_port),address="localhost"), idkey=local_id)
+                node = stub.FindNode(obj)
+                print("we got")
+                print(node)
+                # print(node.responding_node.port)
+
         if (arguments[0] == "STORE"):
             print("handle store here")
         if (arguments[0] == "FIND_VALUE"):
             print("handle find_value here")
         if (arguments[0] == "QUIT"):
             print("handle quit here")
+
 
     
 
